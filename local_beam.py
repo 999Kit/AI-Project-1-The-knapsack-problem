fc032816919a1e773c8c49212ec86fce52aa26ea
Kiet Nguyen <kietnguyentuan911@gmail.com>
2023-03-31 06:50:34 +0700

    done and tested

--- local_beam.py
+++ local_beam.py
@@ -6,16 +6,37 @@ from random import randint
 
 def local_beam(problem):
 
-    def generate_state(problem):
+    def evaluation_function(model):
+        classes_chosen = 0
+        total_weight = 0
+        total_value = 0
+
+        for idx in range(len(problem.w)):
+            if (model >> idx) & 1 == 1:
+                total_weight += problem.w[idx]
+
+                if total_weight > problem.W:
+                    return 0
+
+                total_value += problem.v[idx]
+                classes_chosen |= 1 << (problem.c[idx] - 1)
+
+        if classes_chosen + 1 != 1 << problem.m:
+            return 0
+
+        return total_value
+
+    def generate_state():
         model = 0
         for i in range(len(problem.w)):
             flip = randint(0, 1)
             model |= (flip << i)
-        return (problem.fitness(model) ,model)
+        return (evaluation_function(model) ,model)
 
     beam_width = 5
     def generate_k_states():
-        return [generate_state for _ in range(beam_width)]
+        return [generate_state() for _ in range(beam_width)]
+    
     
 
     def solve(problem):
@@ -25,9 +46,9 @@ def local_beam(problem):
         while beam_depth:
             for i in range(len(problem.w)):
                 for j in range(len(problem.w)):
-                    successor = states[i]
+                    successor = states[i][1]
                     successor ^= (1 << j)
-                    states.append((problem.fitness(successor), successor))
+                    states.append((evaluation_function(successor), successor))
 
             states.sort(reverse=True)
             states = states[:beam_width]
