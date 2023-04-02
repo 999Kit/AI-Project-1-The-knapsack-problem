690a50b6a09387bca041711140a46831a316e79c
Tuan <88710207+tuannbdz@users.noreply.github.com>
2023-04-02 10:50:20 +0700

    clean

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import small_dataset_gen
 from GA import *
 from utils import *
 from brute_force import *
 from branch_and_bound import *
 from local_beam import *
 import time
-import generator
 
 
 def read_data_from_file(file_handle):
@@ -31,9 +29,7 @@ def write_solution_to_file(file_handle, total_value, model, num_of_objects):
 
 
 if __name__ == '__main__':
-    small_dataset_gen.generate_small_dataset()
     num_of_files = int(input("Enter number of input files: "))
-
     for i in range(0, num_of_files):
         with open(f"INPUT_{i}.txt", "r") as input_file, open(f"OUTPUT_{i}.txt", "w") as output_file:
             max_weight, num_of_classes, weight_list, value_list, class_list = read_data_from_file(input_file)
@@ -43,12 +39,11 @@ if __name__ == '__main__':
             start_time = time.time()
             sol, sol_val = branch_and_bound_bfs(kp)
             print(f"--- Running time for branch and bound: %s ms ---" % ((time.time() - start_time) * 1000))
-
             # sol, sol_val = branch_and_bound(kp)
             # ----- Local beam search -------
             # sol, sol_val = local_beam(kp)
             # ----- GA ----------------
             # print(len(kp.w))
-            population = population_generator(kp, 150)
-            sol, sol_val = genetic_algorithm(kp, population, 50)
+            # population = population_generator(kp, 150)
+            # sol, sol_val = genetic_algorithm(kp, population, 50)
             write_solution_to_file(output_file, sol_val, sol, len(weight_list))
